004ffdc2d3ea20384ab32b07a1f1a94a1875d36d
alexkharl <sasha_xarlamov@mail.ru>
2024-10-28 21:22:45 +0300

    Init

---
+++ shell.nix
@@ -0,0 +1,19 @@
+
+{ sources ? import ./npins }:
+let
+  pkgs = import sources.nixpkgs { config = {}; overlays = []; };
+  pythonPackages = pkgs.python3Packages;
+in pkgs.mkShell rec {
+  name = "impurePythonEnv";
+  venvDir = "./.venv";
+  buildInputs = with pkgs; [
+    pythonPackages.numpy
+    pythonPackages.matplotlib
+    pythonPackages.ipykernel
+
+    # A Python interpreter including the 'venv' module is required to bootstrap
+    # the environment.
+    pythonPackages.python
+  ];
+
+}
